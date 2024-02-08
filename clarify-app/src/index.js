63d3c391bc638bf37787a1429c13ed7a9e0e53d8
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-02-08 13:42:24 +0100

    Upload html file for signup page

---
+++ clarify-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+// import "popper.js/dist/umd/popper";
+// import NavBar from "./NavBar";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"))
