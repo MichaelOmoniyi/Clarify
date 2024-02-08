7516a69bc39731766d50dc0f6d4e14a964bd0673
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-02-08 15:44:46 +0100

    Upload necessary files

--- clarify-app/src/SignUpPage.jsx
+++ clarify-app/src/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./SignUp.css"
 
 export default class SignUp extends Component {
   render() {
