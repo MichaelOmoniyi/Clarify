43b018fcdb6a9870743ace34f540ec3a32f5b982
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-02-08 13:45:03 +0100

    Upload main app component

--- clarify-app/src/App.jsx
+++ clarify-app/src/App.jsx
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+
 import { NavBarNew } from "./Navbar";
 import LandingPage from "./LandingPage";
+import SignUp from "./SignUpPage";
 
-export default class App extends Component {
+export class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <NavBarNew />
-        <LandingPage />
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <this.NavBarChecker />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+          </Routes>
+        </React.Fragment>
+      </Router>
     );
+  }
+  NavBarChecker = () => {
+    const navigate = useNavigate();
+    const currentPath = window.location.pathname;
+
+    if (currentPath === "/signup") {
+      return null;
+    }
+
+    return <NavBarNew />;
+  };
+}
+
+export class SignUpPage extends Component {
+  render() {
+    return <SignUp />;
   }
 }
