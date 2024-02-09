bb17150b43f212c6b1d0243979753aa4aa0d5c4f
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-02-09 14:23:57 +0100

    Modify sign up page

--- clarify-app/src/SignUpPage.jsx
+++ clarify-app/src/SignUpPage.jsx
@@ -8,22 +8,24 @@ export default class SignUp extends Component {
   render() {
     return (
       <div className="signUpContainer">
-        <div className="signUpContent">
-          <h3>WELCOME TO CLARIFY</h3>
-          <p>Create an Account</p>
-          <form action="" method="post">
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm Password" />
-            <input type="submit" />
-          </form>
-          <p className="option">or</p>
-          <button>
-            <img className="googleIcon" src={GoogleIcon} alt="Google Icon" />
-            Sign Up with Google
-          </button>
-          <a href="#">Already have an account?</a>
+        <div className="subContainer">
+          <div className="signUpContent">
+            <h3>WELCOME TO CLARIFY</h3>
+            <p>Create an Account</p>
+            <form action="" method="post">
+              <input type="email" placeholder="Email" />
+              <input type="text" placeholder="Username" />
+              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Confirm Password" />
+              <input type="submit" />
+            </form>
+            <p className="option">or</p>
+            <button>
+              <img className="googleIcon" src={GoogleIcon} alt="Google Icon" />
+              Sign Up with Google
+            </button>
+            <a href="#">Already have an account?</a>
+          </div>
         </div>
       </div>
     );
