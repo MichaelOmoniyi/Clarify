fd04fdb220fb120c56596b1c548c474a4d068ebc
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-02-10 16:29:31 +0100

    Update main component file

--- clarify-app/src/App.jsx
+++ clarify-app/src/App.jsx
@@ -7,37 +7,54 @@ import {
 } from "react-router-dom";
 
 import { NavBarUser } from "./Navbar";
+import { NavBarNew } from "./Navbar";
 import LandingPage from "./LandingPage";
 import SignUp from "./SignUpPage";
+import Chat from "./chatpage";
+
+const Title = () => {
+  switch (window.location.pathname) {
+    case "/":
+      document.title = "Home - Clarify";
+      break;
+    case "/signup":
+      document.title = "Sign Up - Clarify";
+      break;
+    case "/chat":
+      document.title = "Chat - Clarify";
+      break;
+    default:
+      document.title = "Clarify";
+  }
+};
+
+const NavBarChecker = () => {
+  const navigate = useNavigate();
+  const currentPath = window.location.pathname;
+
+  if (currentPath === "/signup") {
+    return null;
+  } else if (currentPath === "/chat") {
+    return <NavBarUser />;
+  }
+
+  return <NavBarNew />;
+};
 
 export class App extends Component {
   render() {
     return (
       <Router>
         <React.Fragment>
-          <this.NavBarChecker />
+          <Title />
+          <NavBarChecker />
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/chat" element={<Chat />} />
           </Routes>
         </React.Fragment>
       </Router>
     );
-  }
-  NavBarChecker = () => {
-    const navigate = useNavigate();
-    const currentPath = window.location.pathname;
-
-    if (currentPath === "/signup") {
-      return null;
-    }
-
-    return <NavBarUser />;
-  };
-}
-
-export class SignUpPage extends Component {
-  render() {
-    return <SignUp />;
   }
 }
