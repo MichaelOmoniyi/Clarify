669743c1b778e7a59f19b8ca8ddde07ede57d20f
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-02-10 16:31:39 +0100

    Update project

--- clarify-app/src/SignUp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap";
-import { SignUpPage } from "./App";
-
-ReactDom.render(<SignUpPage />, document.getElementById("signUpRoot"));
--- clarify-app/src/index.js
+++ clarify-app/src/index.js
@@ -4,6 +4,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 // import "popper.js/dist/umd/popper";
 // import NavBar from "./NavBar";
-import {App} from "./App";
+import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
