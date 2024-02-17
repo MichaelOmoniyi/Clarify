e03fd6a452c7e1c78b2d2d5531a7451ca8f4ebfc
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-02-17 11:32:49 +0100

    Stash files

--- clarify-app/src/chatpage.jsx
+++ clarify-app/src/chatpage.jsx
@@ -4,18 +4,26 @@ import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import "./chat.css";
 
 export default class Chat extends Component {
+  prompt = () => {
+    let promptDisplay = document.querySelector(".send");
+    let promptValue = document.getElementById("prompt").value;
+
+    promptDisplay.innerHTML = promptValue;
+    promptValue = ""
+  }
+  
   render() {
     return (
       <div className="chatContainer">
         <div className="chatArea">
           <div className="sendResponse">
-            <div className="response"></div>
+            <div className="response">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In voluptate quidem, iusto, dolorem error, omnis recusandae quasi commodi amet numquam eligendi perspiciatis. Neque, est quidem alias ad corporis in asperiores?</div>
             <div className="send"></div>
           </div>
           <div className="prompt">
-            <form action="#" method="post" className="promptForm">
-              <input type="text" placeholder="Enter prompt..." />
-              <button type="submit">
+            <form action="#" method="get" className="promptForm">
+              <input id="prompt" type="text" placeholder="Enter prompt..." />
+              <button type="submit" onClick={this.prompt}>
                 <FontAwesomeIcon className="upArrow" icon={faArrowUp} />
               </button>
             </form>
